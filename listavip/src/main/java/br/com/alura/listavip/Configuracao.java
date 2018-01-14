48761588235f2fbd90541f1f8fd3f10ee6fb162d
David Vasconcelos <dsv10@hotmail.com>
2018-01-14 16:25:22 -0200

    Final - Properties, FatJar e Actuator

--- listavip/src/main/java/br/com/alura/listavip/Configuracao.java
+++ listavip/src/main/java/br/com/alura/listavip/Configuracao.java
@@ -1,14 +1,7 @@
 package br.com.alura.listavip;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class Configuracao {
@@ -16,17 +9,4 @@ public class Configuracao {
 	public static void main(String[] args) {
 		SpringApplication.run(Configuracao.class, args);
 	}
-	
-	@Bean
-	public DataSource dataSource(){
-	    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-	    dataSource.setUrl("jdbc:mysql://localhost:3306/listavip");
-	    dataSource.setUsername("root");
-	    dataSource.setPassword("alura");
-	    return dataSource;
-	}
-	
-	
-
 }
