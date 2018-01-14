112e0929da01b307060e8376793ae90ef974342c
David Vasconcelos <dsv10@hotmail.com>
2018-01-14 11:50:31 -0200

    Camada de service e email através do Spring Initializr

--- listavip/src/main/java/br/com/alura/listavip/controllers/ConvidadoController.java
+++ listavip/src/main/java/br/com/alura/listavip/controllers/ConvidadoController.java
@@ -4,28 +4,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.alura.listavip.models.Convidado;
-import br.com.alura.listavip.repository.ConvidadoRepository;
+import br.com.alura.listavip.service.ConvidadoService;
 
 @Controller
 public class ConvidadoController {
-	
+
 	@Autowired
-	private ConvidadoRepository repository;
-	
+	private ConvidadoService service;
+
 	@RequestMapping("/")
 	private String index() {
 		return "index";
 	}
-	
-	
+
 	@RequestMapping("listavip")
 	private String listaConvidados(Model model) {
-		Iterable<Convidado> convidados = repository.findAll();
+		Iterable<Convidado> convidados = service.obterTodos();
+		model.addAttribute("convidados", convidados);
+
+		return "listaconvidados";
+	}
+
+	@RequestMapping(value = "salvar", method = RequestMethod.POST)
+	public String salvar(@RequestParam("nome") String nome, @RequestParam("email") String email,
+			@RequestParam("telefone") String telefone, Model model) {
+
+		Convidado novoConvidado = new Convidado(nome, email, telefone);
+		service.salvar(novoConvidado);
+		
+		//new EmailService().enviar(nome, email);
+		
+		Iterable<Convidado> convidados = service.obterTodos();
 	    model.addAttribute("convidados", convidados);
 
-	    return "listaconvidados";
+		return "listaconvidados";
+	}
+	
+	@RequestMapping("pesquisar")
+	private String pesquisar(@RequestParam("nome") String nome, Model model) {
+		Iterable<Convidado> convidados = service.obterPorNome(nome);
+		model.addAttribute("convidados", convidados);
+
+		return "listaconvidados";
 	}
 
 }
--- listavip/src/main/java/br/com/alura/listavip/models/Convidado.java
+++ listavip/src/main/java/br/com/alura/listavip/models/Convidado.java
@@ -13,8 +13,16 @@ public class Convidado {
     private String nome;
     private String email;
     private String telefone;
+    
+    public Convidado(){}
 
-    public Long getId() {
+    public Convidado(String nome, String email, String telefone) {
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+	public Long getId() {
         return id;
     }
 
